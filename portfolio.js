ef02eb18fb70fd4995bfacf2bd229f2052b312f2
Kunal Goyal <80157692+Kunal1701@users.noreply.github.com>
2022-07-09 17:46:25 +0530

    Add files via upload
    Responsive Personal Portfolio website using HTML, CSS, JavaScript

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+document.querySelector('.ham2').addEventListener('click',()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+})
+document.querySelector('.btn').addEventListener('click',()=>{
+    alert("Thanks For Your Response!");
+})
